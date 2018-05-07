f96bbee89ea618c13e59dd1167fcb2d9ae58cde9
hwc <1729116521@qq.com>
2018-05-07 23:15:59 +0800

    add marshall json string to json node

---
+++ json/jackson/json-example/src/test/java/cn/org/aris/json/jackson/converttojsonnode/MarshallStringToJsonNode.java
@@ -0,0 +1,102 @@
+package cn.org.aris.json.jackson.converttojsonnode;
+
+import static cn.org.aris.java.common.Consants.WINDOWS_NEWLINE;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.org.apache.xpath.internal.operations.Equals;
+
+import cn.org.aris.java.log.LogConfig;
+
+public class MarshallStringToJsonNode {
+	
+	private static final Logger logger;
+	private static final String resources;
+	
+	static {
+		resources = MarshallStringToJsonNode.class.getClassLoader().getResource("").getFile().toString();
+		LogConfig.setLogPath(resources + "json/jackson/json-example/");
+		logger = LoggerFactory.getLogger(MarshallStringToJsonNode.class);
+	}
+	
+	/**
+	 * Simply use ObjectMapper.readTree method to convert
+	 * json string into json node
+	 */
+	@Test
+	public void parseJsonStringToJsonNode_simplyUsingObjectMapper() {
+		StringBuilder jsonBuilder = new StringBuilder();
+		jsonBuilder.append("{");
+		jsonBuilder.append(" \"userName\" : \"xiaohu\", ");
+		jsonBuilder.append(" \"password\" : \"123456\" ");
+		jsonBuilder.append("}");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode actualObj = mapper.readTree(jsonBuilder.toString());
+			logger.info("Original Json String is: " + jsonBuilder.toString());
+			logger.info("The Json node is: " + actualObj.toString());
+			logger.info(WINDOWS_NEWLINE);
+			assertNotNull(actualObj);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Parsing Json String to Json Node reading content from a Json Parser
+	 */
+	@Test
+	public void parseJsonStringToJsonNode_usingLowLevelApi() {
+		StringBuilder jsonBuilder = new StringBuilder();
+		jsonBuilder.append("{");
+		jsonBuilder.append(" \"userName\" : \"xiaohu\", ");
+		jsonBuilder.append(" \"password\" : \"123456\" ");
+		jsonBuilder.append("}");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonParser parser = mapper.getFactory().createParser(jsonBuilder.toString());
+			JsonNode actualObj = mapper.readTree(parser);
+			logger.info("Original Json String is: " + jsonBuilder.toString());
+			logger.info("The Json Node is: " + actualObj.toString());
+			logger.info(WINDOWS_NEWLINE);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Using Json Node
+	 */
+	@Test
+	public void usingJsonNode() {
+		StringBuilder jsonBuilder = new StringBuilder();
+		jsonBuilder.append("{");
+		jsonBuilder.append(" \"userName\" : \"xiaohu\", ");
+		jsonBuilder.append(" \"password\" : \"123456\" ");
+		jsonBuilder.append("}");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode actualObj = mapper.readTree(jsonBuilder.toString());
+			JsonNode jsonNodel = actualObj.get("userName");
+			assertThat(jsonNodel.textValue(), equalTo("xiaohu"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- json/jackson/json-example/src/test/java/cn/org/aris/json/jackson/objectmapper/ReadingWritingUsingObjectMapper.java
+++ json/jackson/json-example/src/test/java/cn/org/aris/json/jackson/objectmapper/ReadingWritingUsingObjectMapper.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
