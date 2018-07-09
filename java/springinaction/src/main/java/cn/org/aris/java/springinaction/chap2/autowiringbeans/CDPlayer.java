591ddc768c344b5a64afbe5e0aa9848a81d9aca5
hwc <1729116521@qq.com>
2018-07-10 00:17:48 +0800

    增加了bean自动装配的内容

---
+++ java/springinaction/src/main/java/cn/org/aris/java/springinaction/chap2/autowiringbeans/CDPlayer.java
@@ -0,0 +1,20 @@
+package cn.org.aris.java.springinaction.chap2.autowiringbeans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CDPlayer implements MediaPlayer{
+	private CompactDisc cd;
+	
+	@Autowired
+	public CDPlayer(CompactDisc cd) {
+		this.cd = cd;
+	}
+	
+	@Override
+	public void paly() {
+		cd.play();
+	}
+
+}
---
+++ java/springinaction/src/main/java/cn/org/aris/java/springinaction/chap2/autowiringbeans/MediaPlayer.java
@@ -0,0 +1,9 @@
+package cn.org.aris.java.springinaction.chap2.autowiringbeans;
+
+/**
+ * 
+ * @author ArisHu
+ */
+public interface MediaPlayer {
+	void paly();
+}
