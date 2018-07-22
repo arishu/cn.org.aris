cb5be48672bd716ad6ae6d1a96fdffce801c979e
hwc <1729116521@qq.com>
2018-07-23 00:54:33 +0800

    增加了如何写controller的内容

---
+++ java/spittr/src/main/java/cn/org/aris/java/spittr/data/SpittleRepository.java
@@ -0,0 +1,16 @@
+package cn.org.aris.java.spittr.data;
+
+import java.util.List;
+
+import cn.org.aris.java.spittr.Spittle;
+
+public interface SpittleRepository {
+	
+	List<Spittle> findRecentSpittles();
+	
+	List<Spittle> findSpittles(long max, int count);
+	
+	Spittle findOne(long id);
+	
+	void save(Spittle spittle);
+}
