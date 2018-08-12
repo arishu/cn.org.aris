55c7ee2cd0bba4e1bcdffc590a655d7449f7c868
hwc <1729116521@qq.com>
2018-08-13 01:32:51 +0800

    添加了视图与视图的解析、静态资源的配置使用、多部分请求的解析
    - 视图与视图的解析
    view接口中的render方法，作为view对象的主要功能，就是定义view应该根据给定的Model和request(HttpServletRequest)来将内容渲染到response(HttpServletResponse)中。
    Spring MVC提供了许多方便的View实现类，只需要在web application context配置类中简单地做一些配置，就能够使用这些类。
    
    在web应用程序中，有两种技术可以将页面定位到另一个页面中去：URL重定向和URL前转。
    大多数时候，URL重定向或前转这种技术，会在提交web表单之后被使用，这样做是为了解决由于浏览器回退按钮或F5刷新因素所导致的重复提交表单数据的问题
    forward请求仍然是活跃的原始请求，因此在一开始，放进model中的参数仍然有效；
    当使用了redirect前缀，此时，Spring会把这个请求当作新请求，因此一开始我们放进model中的参数就会丢失；
    
    为了解决URL重定向所带来的Model中参数丢失问题，Spring提供了Flash属性用来保存源请求中的参数。
    Flash属性是临时保存在某个session中，只能在redirect请求发出后立即发出请求才能获取到。
    为了能够使用Flash属性，只需要在原始请求的controller方法参数列表中添加RedirectAttributes redirectAttributes参数
    
    - 静态资源的配置与使用
    有时候，我们希望显示一些图片，文档之类的资源，而不希望请求被controller方法处理，这时候就需要用来静态资源的配置
    首先在web application context配置类中进行配置，具体就是覆盖addResourceHandlers(ResourceHandlerRegistry registry)方法，后使用registry变量进行资源注册
    然后根据配置将相应的资源放在对应的目录下(一般放在src/main/webapp/目录下)。
    
    - 多部分请求的使用
    有时候，我们需要将某些东西上传到应用服务器中，这时候就需要使用多请求
    
    多请求是一种用来将多个文件和数据发送到服务器的HTTP请求。

---
+++ java/springmvc/chap5/redirectview/src/main/java/cn/org/aris/java/springmvc/webstore/config/RootApplicationContextConfig.java
@@ -0,0 +1,30 @@
+package cn.org.aris.java.springmvc.webstore.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@ComponentScan({"cn.org.aris.java.springmvc.webstore"})
+public class RootApplicationContextConfig {
+
+	/*@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setScriptEncoding("UTF-8")
+				.setType(EmbeddedDatabaseType.HSQL)
+				.addScript("db/sql/create_table.sql")
+				.addScript("db/sql/insert_data.sql")
+				.build();
+	}
+	
+	@Bean
+	public NamedParameterJdbcTemplate getJdbcTemplate() {
+		return new NamedParameterJdbcTemplate(dataSource());
+	}*/
+}
